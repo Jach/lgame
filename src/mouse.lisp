517c8c1a5b20aeb78f2803a9a66813750de18ec7
Kevin Secretan <git@thejach.com>
2022-04-14 17:01:43 -0700

    Add clarifying doc on behavior

--- src/mouse.lisp
+++ src/mouse.lisp
@@ -5,7 +5,10 @@
 @export
 (defun get-mouse-pos ()
   "Simple wrapper around 'sdl2:mouse-state, returns the current mouse's position
-   as an x,y list."
+   as an x,y list.
+   Note that this is the position relative to the actual window size, NOT the
+   logical render size. If you need the latter, consider getting the :button :x and :y
+   values from the mouse event, which should be relative to the logical size."
   (multiple-value-bind (x y) (sdl2:mouse-state)
     (list x y)))
 
