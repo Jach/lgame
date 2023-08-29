95b070cd39c4bbab6f7509a23a7b8b9c96461b22
Kevin Secretan <git@thejach.com>
2023-08-28 17:43:57 -0700

    Add some wrappers for setting the mouse cursor to hover/normal

--- src/mouse.lisp
+++ src/mouse.lisp
@@ -2,6 +2,9 @@
 
 (annot:enable-annot-syntax)
 
+(defvar *hover-cursor* nil)
+(defvar *normal-cursor* nil)
+
 @export
 (defun get-mouse-pos ()
   "Simple wrapper around 'sdl2:mouse-state, returns the current mouse's position
@@ -11,4 +14,23 @@
    values from the mouse event, which should be relative to the logical size."
   (multiple-value-bind (x y) (sdl2:mouse-state)
     (list x y)))
+
+@export
+(defun set-hover-cursor ()
+  (unless *hover-cursor*
+    (setf *hover-cursor* (lgame::sdl-create-system-cursor lgame::+sdl-system-cursor-hand+)))
+  (lgame::sdl-set-cursor *hover-cursor*))
+
+@export
+(defun set-normal-cursor ()
+  (unless *normal-cursor*
+    (setf *normal-cursor* (lgame::sdl-create-system-cursor lgame::+sdl-system-cursor-arrow+)))
+  (lgame::sdl-set-cursor *normal-cursor*))
+
+@export
+(defun cleanup-cursors ()
+  (when *hover-cursor*
+    (lgame::sdl-free-cursor *hover-cursor*))
+  (when *normal-cursor*
+    (lgame::sdl-free-cursor *normal-cursor*)))
 
