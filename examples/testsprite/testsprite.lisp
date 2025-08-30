5c50baf0403d139a7015fbfef2246a8b734f3908
Kevin Secretan <git@thejach.com>
2025-08-30 01:49:09 -0700

    Fix up test sprite example, part of #3

--- examples/testsprite/testsprite.lisp
+++ examples/testsprite/testsprite.lisp
@@ -14,18 +14,44 @@ However, it took Lisp about 1 minute and 20 seconds to load up the sprites,
 |#
 
 
-(ql:quickload :lgame)
+;; quicklisp preamble and quickloading for script usage
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload :lgame))
 
 (defpackage #:lgame.example.testsprite
   (:use #:cl)
-  (:export #:main))
+  (:export #:main)
+  (:import-from #:lgame.sprite
+                #:sprite
+                #:.image
+                #:.box
+                #:update
+                #:add-sprites
+                #:group
+                #:draw)
+  (:import-from #:lgame.box
+                #:move-box
+                #:box-x
+                #:box-y
+                #:get-texture-box)
+  (:import-from #:lgame.loader
+                #:get-texture
+                #:create-texture-loader)
+  (:import-from #:lgame.event
+                #:event-type))
 (in-package #:lgame.example.testsprite)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defvar *source-dir* (directory-namestring
                          (or *compile-file-pathname* *load-truename*))))
+
 (defparameter +screen-size+ '(640 480))
-(defparameter *running?* t)
 
 (defparameter *total-sprites* 100)
 
@@ -33,48 +59,51 @@ However, it took Lisp about 1 minute and 20 seconds to load up the sprites,
   "Random int between a and b, inclusive"
   (+ a (random (1+ (- b a)))))
 
-(defclass thingy (lgame.sprite:sprite)
+(defclass thingy (sprite)
   ((vel :accessor .vel :initform (list (rand[] -1 1) (rand[] -1 1)))))
 
 (defmethod initialize-instance :after ((self thingy) &key)
-  (setf (lgame.sprite:.image self) (lgame.loader:get-texture "asprite.bmp" :color-key '(255 255 255))
-        (lgame.sprite:.rect self) (lgame.rect:get-texture-rect (lgame.sprite:.image self)))
-  (lgame.rect:move-rect (lgame.sprite:.rect self) (rand[] 0 (first +screen-size+)) (rand[] 0 (second +screen-size+))))
+  (setf (.image self) (get-texture "asprite.bmp" :color-key '(255 255 255))
+        (.box self) (get-texture-box (.image self)))
+  (move-box (.box self) (rand[] 0 (first +screen-size+)) (rand[] 0 (second +screen-size+))))
 
-(defmethod lgame.sprite:update ((self thingy))
+(defmethod update ((self thingy))
   (dotimes (i 2)
-    (let* ((rect (lgame.sprite:.rect self))
-           (nv (+ (if (zerop i) (sdl2:rect-x rect) (sdl2:rect-y rect)) (elt (.vel self) i))))
+    (let* ((box (.box self))
+           (nv (+ (if (zerop i) (box-x box) (box-y box)) (elt (.vel self) i))))
       (when (or (>= nv (elt +screen-size+ i)) (minusp nv))
         (setf (elt (.vel self) i) (- (elt (.vel self) i)))
-        (setf nv (+ (if (zerop i) (sdl2:rect-x rect) (sdl2:rect-y rect)) (elt (.vel self) i))))
-      (if (zerop i) (setf (sdl2:rect-x rect) nv) (setf (sdl2:rect-y rect) nv)))))
+        (setf nv (+ (if (zerop i) (box-x box) (box-y box)) (elt (.vel self) i))))
+      (if (zerop i) (setf (box-x box) nv) (setf (box-y box) nv)))))
 
 
 (defun main (&aux sprites frames)
   (lgame:init)
-  (lgame.loader:create-texture-loader *source-dir*)
+  (create-texture-loader *source-dir*)
+
   (lgame.display:create-centered-window "Testsprite" (first +screen-size+) (second +screen-size+))
   (lgame.display:create-renderer)
 
-  (setf sprites (make-instance 'lgame.sprite:group))
+  (setf sprites (make-instance 'group))
   (dotimes (i *total-sprites*)
-    (lgame.sprite:add-sprites sprites (make-instance 'thingy)))
+    (add-sprites sprites (make-instance 'thingy)))
 
   (setf frames 0)
   (lgame.time:clock-start)
-  (setf *running?* t)
-  (loop while *running?* do
-        (lgame.event:do-event (event)
-          (if (find (lgame.event:event-type event) `(,lgame::+sdl-quit+ ,lgame::+sdl-keydown+))
-              (setf *running?* nil)))
-        (incf frames)
-        (sdl2:render-clear lgame:*renderer*)
-        (lgame.sprite:update sprites)
-        (lgame.sprite:draw sprites)
-        (sdl2:render-present lgame:*renderer*))
-  (format t "FPS: ~a~%" (/ frames (/ (lgame.time:clock-time) 1000.0)))
-  (lgame:quit))
+  (unwind-protect
+    (loop while (lgame.time:clock-running?) do
+          (lgame.event:do-event (event)
+            (when (find (event-type event) `(,lgame::+sdl-quit+ ,lgame::+sdl-keydown+))
+              (lgame.time:clock-stop)))
+          (incf frames)
+          (lgame.render:clear)
+          (update sprites)
+          (draw sprites)
+          (lgame.render:present))
+
+    (progn
+      (format t "FPS: ~a~%" (/ frames (/ (lgame.time:clock-time) 1000.0)))
+      (lgame:quit))))
 
 (eval-when (:execute)
   (main))
