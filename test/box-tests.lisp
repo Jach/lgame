c04dfa743f06c9ab593ed30ac6bb0e989492b867
Kevin Secretan <git@thejach.com>
2025-09-03 21:40:24 -0700

    Fix box tests

--- test/box-tests.lisp
+++ test/box-tests.lisp
@@ -1,5 +1,10 @@
 (defpackage #:lgame.test.box-tests
   (:use #:cl #:fiveam #:lgame.box)
+  (:import-from #:lgame.box
+                 #:.min-x
+                 #:.min-y
+                 #:.max-x
+                 #:.max-y)
   (:export #:run-box-tests))
 (in-package #:lgame.test.box-tests)
 
@@ -20,29 +25,29 @@
 (test box-creation-and-basic-accessors
   (let ((b1 (make-box 10 20 30 40))
         (b2 (make-box-from-minmax 5 15 55 115)))
-    (is (= 10 (box-min-x b1)))
-    (is (= 20 (box-min-y b1)))
-    (is (= 40 (box-max-x b1))) ; 10 + 30
-    (is (= 60 (box-max-y b1))) ; 20 + 40
+    (is (= 10 (.min-x b1)))
+    (is (= 20 (.min-y b1)))
+    (is (= 40 (.max-x b1))) ; 10 + 30
+    (is (= 60 (.max-y b1))) ; 20 + 40
 
     (is (= 10 (box-x b1)))
     (is (= 20 (box-y b1)))
     (is (= 30 (box-width b1)))
     (is (= 40 (box-height b1)))
 
-    (is (= 5 (box-min-x b2)))
-    (is (= 15 (box-min-y b2)))
-    (is (= 55 (box-max-x b2)))
-    (is (= 115 (box-max-y b2)))
+    (is (= 5 (.min-x b2)))
+    (is (= 15 (.min-y b2)))
+    (is (= 55 (.max-x b2)))
+    (is (= 115 (.max-y b2)))
     (is (= 50 (box-width b2)))
     (is (= 100 (box-height b2)))))
 
 (test box-creation-with-floats
   (let ((b (make-box 10.5 20.25 30.5 40.75)))
-    (is (= 10.5 (box-min-x b)))
-    (is (= 20.25 (box-min-y b)))
-    (is (approx= 41.0 (box-max-x b)))    ; 10.5 + 30.5
-    (is (approx= 61.0 (box-max-y b)))    ; 20.25 + 40.75
+    (is (= 10.5 (.min-x b)))
+    (is (= 20.25 (.min-y b)))
+    (is (approx= 41.0 (.max-x b)))    ; 10.5 + 30.5
+    (is (approx= 61.0 (.max-y b)))    ; 20.25 + 40.75
     (is (approx= 30.5 (box-width b)))
     (is (approx= 40.75 (box-height b)))))
 
@@ -50,37 +55,37 @@
   (let* ((b1 (make-box 10 20 30 40))
          (b2 (copy-box b1)))
     (is (not (eq b1 b2))) ; Should be different objects
-    (is (= (box-min-x b1) (box-min-x b2)))
-    (is (= (box-min-y b1) (box-min-y b2)))
-    (is (= (box-max-x b1) (box-max-x b2)))
-    (is (= (box-max-y b1) (box-max-y b2)))))
+    (is (= (.min-x b1) (.min-x b2)))
+    (is (= (.min-y b1) (.min-y b2)))
+    (is (= (.max-x b1) (.max-x b2)))
+    (is (= (.max-y b1) (.max-y b2)))))
 
 ;;; Tests for (setf box-x/y/width/height)
 (test box-setf-basic-accessors
   (let ((b (make-box 0 0 10 20)))
     (setf (box-x b) 5)
-    (is (= 5 (box-min-x b)))
-    (is (= 15 (box-max-x b))) ; width 10 preserved
+    (is (= 5 (.min-x b)))
+    (is (= 15 (.max-x b))) ; width 10 preserved
     (is (= 10 (box-width b)))
 
     (setf (box-y b) 7)
-    (is (= 7 (box-min-y b)))
-    (is (= 27 (box-max-y b))) ; height 20 preserved
+    (is (= 7 (.min-y b)))
+    (is (= 27 (.max-y b))) ; height 20 preserved
     (is (= 20 (box-height b)))
 
     (setf (box-width b) 100)
-    (is (= 5 (box-min-x b)))   ; min-x preserved
-    (is (= 105 (box-max-x b)))
+    (is (= 5 (.min-x b)))   ; .min-x preserved
+    (is (= 105 (.max-x b)))
     (is (= 100 (box-width b)))
 
     (setf (box-height b) 200)
-    (is (= 7 (box-min-y b)))   ; min-y preserved
-    (is (= 207 (box-max-y b)))
+    (is (= 7 (.min-y b)))   ; .min-y preserved
+    (is (= 207 (.max-y b)))
     (is (= 200 (box-height b)))))
 
 ;;; Tests for box-attr getter
 (test box-attr-getters
-  (let ((b (make-box 10 20 30 40))) ; min-x=10, min-y=20, max-x=40, max-y=60
+  (let ((b (make-box 10 20 30 40))) ; .min-x=10, .min-y=20, .max-x=40, .max-y=60
     (is (= 10 (box-attr b :x)))
     (is (= 20 (box-attr b :y)))
     (is (= 30 (box-attr b :width)))
@@ -112,12 +117,12 @@
     (setf (box-attr b :left) 10)
     (is (= 10 (box-x b)))
     (is (= 100 (box-width b))) ; width preserved
-    (is (= 110 (box-max-x b)))))
+    (is (= 110 (.max-x b)))))
 
 (test box-attr-setf-right
   (let ((b (make-box 0 0 100 200)))
     (setf (box-attr b :right) 50)
-    (is (= 50 (box-max-x b)))
+    (is (= 50 (.max-x b)))
     (is (= 100 (box-width b)) "width preserved")
     (is (= -50 (box-x b)))))
 
@@ -126,36 +131,36 @@
     (setf (box-attr b :top) 20)
     (is (= 20 (box-y b)))
     (is (= 200 (box-height b))) ; height preserved
-    (is (= 220 (box-max-y b)))))
+    (is (= 220 (.max-y b)))))
 
 (test box-attr-setf-bottom
   (let ((b (make-box 0 0 100 200)))
     (setf (box-attr b :bottom) 60)
-    (is (= 60 (box-max-y b)))
+    (is (= 60 (.max-y b)))
     (is (= 200 (box-height b))) ; height preserved
     (is (= -140 (box-y b)))))
 
 (test box-attr-setf-centerx
-  (let ((b (make-box 0 0 10 20))) ; min-x=0, min-y=0, max-x=10, max-y=20
+  (let ((b (make-box 0 0 10 20))) ; .min-x=0, .min-y=0, .max-x=10, .max-y=20
     (setf (box-attr b :centerx) 10)
-    (is (= 5 (box-min-x b)))   ; 10 - 10/2
-    (is (= 15 (box-max-x b)))  ; 10 + 10/2
+    (is (= 5 (.min-x b)))   ; 10 - 10/2
+    (is (= 15 (.max-x b)))  ; 10 + 10/2
     (is (= 10 (box-width b))))) ; width preserved
 
 (test box-attr-setf-centery
   (let ((b (make-box 0 0 10 20)))
     (setf (box-attr b :centery) 15)
-    (is (= 5 (box-min-y b)))   ; 15 - 20/2
-    (is (= 25 (box-max-y b)))  ; 15 + 20/2
+    (is (= 5 (.min-y b)))   ; 15 - 20/2
+    (is (= 25 (.max-y b)))  ; 15 + 20/2
     (is (= 20 (box-height b))))) ; height preserved
 
 (test box-attr-setf-center
   (let ((b (make-box 0 0 10 20)))
     (setf (box-attr b :center) '(30 40))
-    (is (= 25 (box-min-x b)))  ; 30 - 10/2
-    (is (= 35 (box-max-x b)))  ; 30 + 10/2
-    (is (= 30 (box-min-y b)))  ; 40 - 20/2
-    (is (= 50 (box-max-y b)))  ; 40 + 20/2
+    (is (= 25 (.min-x b)))  ; 30 - 10/2
+    (is (= 35 (.max-x b)))  ; 30 + 10/2
+    (is (= 30 (.min-y b)))  ; 40 - 20/2
+    (is (= 50 (.max-y b)))  ; 40 + 20/2
     (is (= 10 (box-width b)))
     (is (= 20 (box-height b)))))
 
@@ -166,82 +171,82 @@
     (is (= 60 (box-y b)))
     (is (= 10 (box-width b)))   ; width/height preserved
     (is (= 20 (box-height b)))
-    (is (= 60 (box-max-x b)))
-    (is (= 80 (box-max-y b)))))
+    (is (= 60 (.max-x b)))
+    (is (= 80 (.max-y b)))))
 
 (test box-attr-setf-bottomleft
-  (let ((b (make-box 0 0 10 20))) ; min-x=0, min-y=0, max-x=10, max-y=20
+  (let ((b (make-box 0 0 10 20))) ; .min-x=0, .min-y=0, .max-x=10, .max-y=20
     (setf (box-attr b :bottomleft) '(5 25))
     (is (= 5 (box-x b)))        ; left is 5
-    (is (= 25 (box-max-y b)))   ; bottom is 25
+    (is (= 25 (.max-y b)))   ; bottom is 25
     (is (= 10 (box-width b)))   ; width preserved
     (is (= 20 (box-height b)))  ; height preserved
     (is (= 5 (box-y b)))        ; 25 - 20
-    (is (= 15 (box-max-x b))))) ; 5 + 10
+    (is (= 15 (.max-x b))))) ; 5 + 10
 
 (test box-attr-setf-topright
-  (let ((b (make-box 0 0 10 20))) ; min-x=0, min-y=0, max-x=10, max-y=20
+  (let ((b (make-box 0 0 10 20))) ; .min-x=0, .min-y=0, .max-x=10, .max-y=20
     (setf (box-attr b :topright) '(15 5))
-    (is (= 15 (box-max-x b)))   ; right is 15
+    (is (= 15 (.max-x b)))   ; right is 15
     (is (= 5 (box-y b)))        ; top is 5
     (is (= 10 (box-width b)))   ; width preserved
     (is (= 20 (box-height b)))  ; height preserved
     (is (= 5 (box-x b)))        ; 15 - 10
-    (is (= 25 (box-max-y b))))) ; 5 + 20
+    (is (= 25 (.max-y b))))) ; 5 + 20
 
 (test box-attr-setf-bottomright
-  (let ((b (make-box 0 0 10 20))) ; min-x=0, min-y=0, max-x=10, max-y=20
+  (let ((b (make-box 0 0 10 20))) ; .min-x=0, .min-y=0, .max-x=10, .max-y=20
     (setf (box-attr b :bottomright) '(15 25))
-    (is (= 15 (box-max-x b)))   ; right is 15
-    (is (= 25 (box-max-y b)))   ; bottom is 25
+    (is (= 15 (.max-x b)))   ; right is 15
+    (is (= 25 (.max-y b)))   ; bottom is 25
     (is (= 10 (box-width b)))   ; width preserved
     (is (= 20 (box-height b)))  ; height preserved
     (is (= 5 (box-x b)))        ; 15 - 10
     (is (= 5 (box-y b)))))      ; 25 - 20
 
 (test box-attr-setf-midtop
-  (let ((b (make-box 0 0 10 20))) ; min-x=0, min-y=0, max-x=10, max-y=20
+  (let ((b (make-box 0 0 10 20))) ; .min-x=0, .min-y=0, .max-x=10, .max-y=20
     (setf (box-attr b :midtop) '(10 5))
     (is (= 10 (box-attr b :centerx)))
     (is (= 5 (box-y b)))        ; top is 5
     (is (= 10 (box-width b)))   ; width preserved
     (is (= 20 (box-height b)))  ; height preserved
     (is (= 5 (box-x b)))        ; centerx 10 - width/2 (5)
-    (is (= 15 (box-max-x b)))   ; centerx 10 + width/2 (5)
-    (is (= 25 (box-max-y b))))) ; top 5 + height 20
+    (is (= 15 (.max-x b)))   ; centerx 10 + width/2 (5)
+    (is (= 25 (.max-y b))))) ; top 5 + height 20
 
 (test box-attr-setf-midleft
-  (let ((b (make-box 0 0 10 20))) ; min-x=0, min-y=0, max-x=10, max-y=20
+  (let ((b (make-box 0 0 10 20))) ; .min-x=0, .min-y=0, .max-x=10, .max-y=20
     (setf (box-attr b :midleft) '(5 15))
     (is (= 5 (box-x b)))        ; left is 5
     (is (= 15 (box-attr b :centery)))
     (is (= 10 (box-width b)))   ; width preserved
     (is (= 20 (box-height b)))  ; height preserved
     (is (= 5 (box-y b)))        ; centery 15 - height/2 (10)
-    (is (= 25 (box-max-y b)))   ; centery 15 + height/2 (10)
-    (is (= 15 (box-max-x b))))) ; left 5 + width 10
+    (is (= 25 (.max-y b)))   ; centery 15 + height/2 (10)
+    (is (= 15 (.max-x b))))) ; left 5 + width 10
 
 (test box-attr-setf-midbottom
-  (let ((b (make-box 0 0 10 20))) ; min-x=0, min-y=0, max-x=10, max-y=20
+  (let ((b (make-box 0 0 10 20))) ; .min-x=0, .min-y=0, .max-x=10, .max-y=20
     (setf (box-attr b :midbottom) '(10 25))
     (is (= 10 (box-attr b :centerx)))
-    (is (= 25 (box-max-y b)))   ; bottom is 25
+    (is (= 25 (.max-y b)))   ; bottom is 25
     (is (= 10 (box-width b)))   ; width preserved
     (is (= 20 (box-height b)))  ; height preserved
     (is (= 5 (box-x b)))        ; centerx 10 - width/2 (5)
-    (is (= 15 (box-max-x b)))   ; centerx 10 + width/2 (5)
+    (is (= 15 (.max-x b)))   ; centerx 10 + width/2 (5)
     (is (= 5 (box-y b)))))      ; bottom 25 - height 20
 
 (test box-attr-setf-midright
-  (let ((b (make-box 0 0 10 20))) ; min-x=0, min-y=0, max-x=10, max-y=20
+  (let ((b (make-box 0 0 10 20))) ; .min-x=0, .min-y=0, .max-x=10, .max-y=20
     (setf (box-attr b :midright) '(15 15))
-    (is (= 15 (box-max-x b)))   ; right is 15
+    (is (= 15 (.max-x b)))   ; right is 15
     (is (= 15 (box-attr b :centery)))
     (is (= 10 (box-width b)))   ; width preserved
     (is (= 20 (box-height b)))  ; height preserved
     (is (= 5 (box-x b)))        ; right 15 - width 10
     (is (= 5 (box-y b)))        ; centery 15 - height/2 (10)
-    (is (= 25 (box-max-y b))))) ; centery 15 + height/2 (10)
+    (is (= 25 (.max-y b))))) ; centery 15 + height/2 (10)
 
 (test box-attr-setf-size
   (let ((b (make-box 10 20 0 0)))
@@ -250,21 +255,21 @@
     (is (= 20 (box-y b)))
     (is (= 70 (box-width b)))
     (is (= 80 (box-height b)))
-    (is (= 80 (box-max-x b)))  ; 10 + 70
-    (is (= 100 (box-max-y b))))) ; 20 + 80
+    (is (= 80 (.max-x b)))  ; 10 + 70
+    (is (= 100 (.max-y b))))) ; 20 + 80
 
 (test box-attr-setf-min-max-direct
   (let ((b (make-box 0 0 10 20)))
     (setf (box-attr b :min-x) 5)
-    (is (= 5 (box-min-x b)))
+    (is (= 5 (.min-x b)))
     (setf (box-attr b :max-x) 25)
-    (is (= 25 (box-max-x b)))
+    (is (= 25 (.max-x b)))
     (is (= 20 (box-width b))) ; width changed as expected
 
     (setf (box-attr b :min-y) 7)
-    (is (= 7 (box-min-y b)))
+    (is (= 7 (.min-y b)))
     (setf (box-attr b :max-y) 27)
-    (is (= 27 (box-max-y b)))
+    (is (= 27 (.max-y b)))
     (is (= 20 (box-height b))))) ; height changed as expected
 
 (test box-attr-setf-x-y-width-height-direct
@@ -277,10 +282,10 @@
     (is (= 20 (box-y b)))
     (is (= 30 (box-width b)))
     (is (= 40 (box-height b)))
-    (is (= 10 (box-min-x b)))
-    (is (= 20 (box-min-y b)))
-    (is (= 40 (box-max-x b)))
-    (is (= 60 (box-max-y b)))))
+    (is (= 10 (.min-x b)))
+    (is (= 20 (.min-y b)))
+    (is (= 40 (.max-x b)))
+    (is (= 60 (.max-y b)))))
 
 ;;; Tests for Collision and Containment
 (test box-collision-and-containment
@@ -314,8 +319,8 @@
   (is-true (box-contains-point? b1 0 0))
   (is-true (box-contains-point? b1 5 5))
   (is-true (box-contains-point? b1 9.99 9.99))
-  (is-false (box-contains-point? b1 10 5))  ; x == max-x
-  (is-false (box-contains-point? b1 5 10))  ; y == max-y
+  (is-false (box-contains-point? b1 10 5))  ; x == .max-x
+  (is-false (box-contains-point? b1 5 10))  ; y == .max-y
   (is-false (box-contains-point? b1 10 10))
   (is-false (box-contains-point? b1 -1 5))
   (is-false (box-contains-point? b1 5 -1))
@@ -364,5 +369,7 @@
 (test print-output
   (let ((b (make-box 10 20 30 40)))
     (is-true (str:starts-with? "#<BOX X:10 Y:20 W:30 H:40 MAX-X:40 MAX-Y:60 "
-                               (write-to-string b)))))
+                               (write-to-string b))
+             "output was not as expected -- was ~a" (write-to-string b))))
+
 
